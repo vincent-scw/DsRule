d9b581ccb279ff9b47eec286241278aafe09b542
Vincent Shen <SHENVIN@chrobinson.com>
2021-10-10 08:15:48 +0800

    add executor

---
+++ src/DsRule/Ast/BinaryOperation/Operations.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsRule.Ast.BinaryOperation
+{
+    class Operations
+    {
+        //private static readonly IDictionary<Operators, >
+    }
+}
---
+++ src/DsRule/DsRuleExecutor.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsRule
+{
+    public static class DsRuleExecutor
+    {
+        public static bool Execute<TIn>(TIn entity, string ruleExpression)
+        {
+            return false;
+        }
+
+        public static bool Execute<TIn1, TIn2>(TIn1 entity1, TIn2 entity2, string ruleExpression)
+        {
+            return false;
+        }
+
+        public static bool Execute<TIn1, TIn2, TIn3>(TIn1 entity1, TIn2 entity2, TIn3 entity3, string ruleExpression)
+        {
+            return false;
+        }
+    }
+}
